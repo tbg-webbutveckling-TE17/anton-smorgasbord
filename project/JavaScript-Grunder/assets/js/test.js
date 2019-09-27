dd49fb4b541e2dfec096dd64103394f9ed6ba8ac
Anton Lilja <antolilj@lycksele.se>
2019-09-27 10:19:30 +0200

    last lessons thingighing

--- project/JavaScript-Grunder/assets/js/test.js
+++ project/JavaScript-Grunder/assets/js/test.js
@@ -1,1 +1,86 @@
-alert("grundberg is so very gay, dicksucking cockeating eggboi")
+
+
+// alert("Pop-Up-Ruta")
+
+var myName = "Anton"; //Datatyp: String
+var myAge = 18; // Datatyp: Number
+var isEvil = false; // Datatyp: Boolean
+
+// Single line comment
+
+/*
+    *************
+    Block comment
+    *************
+*/
+
+/*
+    ----------------
+        if/else
+    ----------------
+ */
+
+ if(myAge < 18) {
+    // alert("You can not drink u faggot.")
+ } else {
+    // alert("You are old enough to drink, still a faggot though.")
+ } 
+
+ if(myName == "Anton") {
+    // alert("Welcome faggot")
+ } else {
+    // alert("GTFO")
+ }
+
+ // == equals, === equals to and same datatype
+
+ if(myAge == "18") {  
+    //  alert("Welcome gay")
+ } else {
+    //  alert("Trashfaggotgayfaggotewwwwwwwwwwwwwwwwwwww")
+ }
+
+ if(isEvil == true) {
+    //  alert("Du är ond")
+ } else {
+    //  alert("Du är god")
+ }
+ 
+ /* 
+    ****************
+      Guessing APP
+    ****************
+
+ */
+
+
+/*  var userInput = Number(prompt("Guess a number between 0 - 10"))
+  
+ document.onload = function() {
+   var secretNumber = 7;
+   var userInput = Number(prompt("Guess a number between 0 - 10"))
+   
+   while(userInput != 7) {
+      if(userInput === secretNumber + 1 || secretNumber -1) {
+         document.writeln("You're close fam.")
+      } else {
+         document.writeln("You're very far away, in a whole nu world.")
+      }
+
+   
+   
+};
+
+if(userInput === secretNumber) {
+   document.writeln("Grattis! Du har gissat rätt. <br>");
+ }
+
+ } */
+
+
+
+
+
+
+ 
+ 
---
+++ project/JavaScript-Grunder/assets/js/test2.js
@@ -0,0 +1,12 @@
+
+
+//Ask the user "Are we there yet?"
+var answer = prompt("Are we there yet?")
+
+// Keep asking again and again until they enter "Yes" OR "Yeah"
+while(answer != "yes" || answer != "yeah") {
+    var answer = prompt("Are we there yet?")
+}
+
+// Then, alert "Yay, we finally made it!"
+alert("We finally made it!")
