8005f91e05edfafebc4f4fa480bc38f455ab2aa8
Anton Lilja <antolilj@lycksele.se>
2019-10-02 12:42:57 +0200

    yesboi

--- project/JavaScript-Grunder/assets/js/test3.js
+++ project/JavaScript-Grunder/assets/js/test3.js
@@ -13,8 +13,42 @@ for(var i = 0; i < str.length; i++) {
     console.log(str[i]);
 }
 
+console.log();
+
 var myArr = ["HTML", "CSS","JS", 42, true];
 var len = myArr.length;
 for (var i = 0; i < len; i++) {
-    console.log(myArr[i]);
+    console.log(myArr[i]); 
+}
+
+console.log()
+
+// Task 1
+for(var i = -10; i <= 19; i++) {
+    console.log(i)
+}
+
+console.log()
+
+// Task 2
+for(var i = 10; i <= 40; i++) {
+    if(i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+// Task 3 
+for(i = 300; i <= 333; i++) {
+    if(i % 2 !== 0 ) {
+        console.log(i)
+
+    }
+}
+
+console.log()
+
+for(var i = 5; i <= 50; i++) {
+    if(i % 3 == 0 && i % 5 == 0) {
+        console.log(i)
+    }
 }
