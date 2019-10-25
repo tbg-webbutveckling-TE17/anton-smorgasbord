da3a6104de090ca04b464ff5b768450381709df7
Anton Lilja <antolilj@lycksele.se>
2019-10-25 11:21:49 +0200

    functions connected with html

--- project/JavaScript-Grunder/assets/js/function.js
+++ project/JavaScript-Grunder/assets/js/function.js
@@ -20,3 +20,64 @@ function kebabToSnake(kebab) {
 kebabToSnake("I-am-a-kebab-cased-string")
 
 console.log(kebabToSnake("I-am-a-kebab-cased-string"))
+
+function localScope() {
+    var showScope = 31;
+    console.log(showScope)
+}
+
+localScope();
+console.log(showScope);
+
+var showScope = "I'm a global variable";
+console.log(showScope);
+
+var showGlobalVar = 99;
+function globalScope() {
+    showGlobalVar = 100;
+    console.log(showGlobalVar)
+}
+
+console.log(showGlobalVar)
+globalScope()
+console.log(showGlobalVar)
+
+function hi() {
+    var name = "Faggot"
+    console.log()
+}
+
+hi()
+
+function bye() {
+    console.log(name)
+}
+
+function sing() {
+    console.log("I'm singing in the rain")
+    console.log("Just singing in the rain")
+    console.log("What a glorious feeling")
+}
+
+fisk = setInterval(sing, 1000);
+clearInterval(fisk);
+
+var intervalExample = setInterval(sing, 2000);
+clearInterval(intervalExample);
+
+ var anon = setInterval(function() {
+     console.log("I'm an anonomous function")
+ }, 1500);
+
+ clearInterval(anon)
+
+ function myTimer() {
+     var d = new Date();
+     var time = d.toLocaleTimeString();
+     document.getElementById("timeOutput").innerHTML = time;
+
+ }
+
+ myTimer();
+
+ var myTime = setInterval(myTimer, 1000)
