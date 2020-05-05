4b76693900e25813c7264c88831be602d94e5435
Anton Lilja <anton_lilja1@hotmail.se>
2020-05-05 15:10:15 +0200

    Fixed cart to pop up

--- project/Webbutveckling 2 - 19-20/Slut Projekt/assets/js/slutProjekt.js
+++ project/Webbutveckling 2 - 19-20/Slut Projekt/assets/js/slutProjekt.js
@@ -2,6 +2,10 @@ var btnPrevImage = document.querySelector(".btnPrev");
 var btnNextImage = document.querySelector(".btnNext");
 var sectionImages = document.querySelector(".images");
 var headerScroll = document.querySelector("header");
+var cartBtn = document.querySelector("#cartBtn");
+var cartXBtn = document.querySelector(".fa-times");
+var cart = document.querySelector("#cart");
+var mainContent = document.querySelector("main");
 
 var marginPx = 0;
 
@@ -29,5 +33,13 @@ btnPrevImage.addEventListener("click", function() {
     
 });
 
+cartBtn.addEventListener("click", function() {
+    cart.style.visibility = "visible";
+    mainContent.style.filter = "blur(30px)";    
+});
 
+cartXBtn.addEventListener("click", function() {
+    cart.style.visibility = "hidden";
+    mainContent.style.filter = "blur(0px)";
+});
 
