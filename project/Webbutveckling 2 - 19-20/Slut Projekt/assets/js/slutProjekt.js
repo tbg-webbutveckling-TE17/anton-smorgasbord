5886dc149926f2878658b2b46a4cfe1124e7ce4e
Anton Lilja <anton_lilja1@hotmail.se>
2020-05-19 13:59:18 +0200

    POGGERS

--- project/Webbutveckling 2 - 19-20/Slut Projekt/assets/js/slutProjekt.js
+++ project/Webbutveckling 2 - 19-20/Slut Projekt/assets/js/slutProjekt.js
@@ -7,6 +7,9 @@ var cartXBtn = document.querySelector(".fa-times");
 var cart = document.querySelector("#cart");
 var mainContent = document.querySelector("main");
 
+
+var goods = [];
+
 var marginPx = 0;
 
 btnNextImage.addEventListener("click", function() {
@@ -45,3 +48,35 @@ cartXBtn.addEventListener("click", function() {
     mainContent.style.filter = "blur(0px)";
 });
 
+var products = [
+    {
+        name: "RTX GEFORCE 2080ti",
+        product: "assets/images/2080ti.png",
+        price: "14.999 kr"
+    },
+    {
+        name: "GTX GEFORCE 1060",
+        product: 1,
+        price: "2.990 kr"
+    },
+    {
+        name: "GTX GEFORCE 1080",
+        product: 1,
+        price: "7.999 kr"
+    },
+    {
+        name: "GTX GEFORCE 970",
+        product: 1,
+        price: "1.099 kr"
+    },
+    {
+        name: "GTX GEFORCE 1070",
+        product: 1,
+        price: "5.390 kr"
+    }
+]
+
+var testImg = document.createElement("img");
+testImg.classList.add("createdImages");
+testImg.src = products[0].product;
+cart.appendChild(testImg);
